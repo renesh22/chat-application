6d3e1a01ae967d72a1380a99e31ee45ae4a6363f
renesh22 <princemehta1008@gmail.com>
2024-04-16 01:06:06 +0530

    done

---
+++ build/precache-manifest.b456f54166e453902cbc53948d4bdd1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4631f7e7462bc0567403396627205801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec8ebe3bcf8c8fe754ab",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "2bb7442112618e4afb23",
+    "url": "/static/js/2.fafa0ab6.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.fafa0ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec8ebe3bcf8c8fe754ab",
+    "url": "/static/js/main.1bfc9b6f.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
